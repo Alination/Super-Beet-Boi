fc99c3b8ef594e2b81053b2c48037a1d33ac1c23
Alination <alinationvarela@gmail.com>
2017-12-07 00:06:43 -0800

    First files!
    Added files with prototype sprites, simple character movement and camera follow smoothing.

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    public int playerJumpPower = 1250;
+
+    private float moveX;
+    private bool facingRight = true;
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //Animations
+        //Player Direction
+        if (moveX < 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 scale = gameObject.transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+}
