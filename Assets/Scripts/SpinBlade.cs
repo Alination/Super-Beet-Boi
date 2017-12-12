b81b4ad80e436aa823c9f8f2e8ecce21724d5ed4
Daniel Savage <dansvg@gmail.com>
2017-12-12 09:28:24 -0800

    Add sawblade prefab

---
+++ Assets/Scripts/SpinBlade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinBlade : MonoBehaviour {
+
+
+	#region Fields - Public
+
+	public float rotationsPerSecond = 2.0f;
+
+	#endregion
+
+	new private Renderer renderer; 
+
+        // Use this for initialization
+	void Start () {
+		this.renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.renderer.transform.RotateAround (this.renderer.transform.position, new Vector3 (0, 0, 1), this.rotationsPerSecond * 1000 * UnityEngine.Time.deltaTime);
+	}
+}
